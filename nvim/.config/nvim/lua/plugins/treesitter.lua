703aad254047e07c6930579c7d70418a861de1c0
blitterated <blitterated@protonmail.com>
2025-05-18 22:24:54 -0500

    Added neovim

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config. setup ({
+      ensure_installed = {
+        "awk",
+        "bash",
+        "c_sharp",
+        "forth",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "ruby",
+        "sql",
+        "supercollider",
+        "tmux",
+        "vim"
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,16 @@
+-- Show line numbers by default
+vim.cmd("set number")
+
+-- Configure Tabs
+vim.cmd([[
+  set expandtab
+  set tabstop=2
+  set softtabstop=2
+  set shiftwidth=2
+]])
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
