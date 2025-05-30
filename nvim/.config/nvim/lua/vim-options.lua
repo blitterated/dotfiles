261339d7ab42fbe6a79b7ada8faecc32b1ed2c25
blitterated <blitterated@protonmail.com>
2025-05-30 15:11:11 -0500

    Turn off neovim search highlighting with Enter key

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -10,7 +10,12 @@ vim.cmd([[
 ]])
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
+-- loading lazy.nvim so that mappings are correct
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- Turn off search highlighting with Enter key
+vim.cmd([[
+  nmap <Enter> :nohlsearch<Enter>/<BS>
+]])
