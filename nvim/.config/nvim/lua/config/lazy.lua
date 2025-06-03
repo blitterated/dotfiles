dc7f97dfd690c6fac4a149ea6ca7e7cb1c27969d
blitterated <blitterated@protonmail.com>
2025-06-03 17:29:21 -0500

    Delete cruft line

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -28,7 +28,6 @@ require("lazy").setup({
     install = {
       { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
     },
-    --{ import = "" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
