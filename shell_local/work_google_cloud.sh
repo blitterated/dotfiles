fc9d1825cc49308624eacfedbe8b19650c287a9f
blitterated <blitterated@protonmail.com>
2025-08-01 17:02:42 -0500

    macOS templating
    Moved google cloud tool config into macos machine config templates.
    Looks for and loads a ~/.secrets file if it exists.
    Changed template code to keep surrounding whitespace.
    Added figlet function for generating template separators.

---
+++ shell_local/work_google_cloud.sh
@@ -0,0 +1,17 @@
+
+# ▄ ▄▖▄▖▄▖▖ ▖  ▄▖▄▖▄▖▄▖▖ ▄▖  ▄▖▖ ▄▖▖▖▄   ▄▖▄▖▄▖▖ ▄▖
+# ▙▘▙▖▌ ▐ ▛▖▌  ▌ ▌▌▌▌▌ ▌ ▙▖  ▌ ▌ ▌▌▌▌▌▌  ▐ ▌▌▌▌▌ ▚
+# ▙▘▙▖▙▌▟▖▌▝▌  ▙▌▙▌▙▌▙▌▙▖▙▖  ▙▖▙▖▙▌▙▌▙▘  ▐ ▙▌▙▌▙▖▄▌
+
+gcsdk_path="${HOME}/google-cloud-sdk"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '${gcsdk_path}/path.zsh.inc' ]; then . '${gcsdk_path}/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '${gcsdk_path}/completion.zsh.inc' ]; then . '${gcsdk_path}/completion.zsh.inc'; fi
+
+# ▄▖▖ ▖▄   ▄▖▄▖▄▖▄▖▖ ▄▖  ▄▖▖ ▄▖▖▖▄   ▄▖▄▖▄▖▖ ▄▖
+# ▙▖▛▖▌▌▌  ▌ ▌▌▌▌▌ ▌ ▙▖  ▌ ▌ ▌▌▌▌▌▌  ▐ ▌▌▌▌▌ ▚
+# ▙▖▌▝▌▙▘  ▙▌▙▌▙▌▙▌▙▖▙▖  ▙▖▙▖▙▌▙▌▙▘  ▐ ▙▌▙▌▙▖▄▌
+
