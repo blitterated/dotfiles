ba34dd52a34aa188dca87545085bd773d7771c92
blitterated <blitterated@proton.me>
2026-02-16 23:20:08 -0600

    Add Wezterm color schemes

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -12,4 +12,28 @@ config.window_decorations = "TITLE | RESIZE"
 -- BlinkingBar, BlinkingBlock, BlinkingUnderline, SteadyBar, SteadyBlock, SteadyUnderline
 config.default_cursor_style = 'BlinkingBlock'
 
+--config.color_scheme = 'Purple People Eater (Gogh)'
+--config.color_scheme = 'Pixiefloss (Gogh)'
+--config.color_scheme = 'Peppermint'
+--config.color_scheme = 'PaperColor Dark (base16)'
+--config.color_scheme = 'Jellybeans (Gogh)'
+--config.color_scheme = 'Abernathy'
+--config.color_scheme = 'Astrodark (Gogh)'
+--config.color_scheme = 'Atelier Lakeside (base16)'
+--config.color_scheme = 'Atelier Seaside (base16)'
+--config.color_scheme = 'Aura (Gogh)'
+--config.color_scheme = 'Aurora'
+--config.color_scheme = 'Japanesque (Gogh)'
+
+
+--config.color_scheme = 'Jellybeans (Gogh)'
+--config.color_scheme = 'JWR dark (terminal.sexy)'
+--config.color_scheme = 'Argonaut (Gogh)'
+--config.color_scheme = 'Atom (Gogh)'
+--config.color_scheme = 'Banana Blueberry'
+--config.color_scheme = 'JetBrains Darcula'
+--config.color_scheme = 'ayu'
+config.color_scheme = 'Ayu Dark (Gogh)'
+--config.color_scheme = 'Bamboo Multiplex'
+
 return config
