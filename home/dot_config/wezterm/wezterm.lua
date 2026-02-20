d933ceceda4099d70ad1dc8d23c9a60cc703ce70
blitterated <blitterated@proton.me>
2026-02-20 16:15:37 -0600

    Update Wezterm config
    Split UI and UX settings into sections.
    Add TITLE back to window_decorations.
    Add and try a bunch of nerd fonts.
    Add a googol billion color schemes to try.

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -2,19 +2,50 @@ local wezterm = require("wezterm")
 local config  = wezterm.config_builder()
 
 config.automatically_reload_config = true
+
+-- UX settings
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.window_close_confirmation = 'NeverPrompt'
 
+-- UI settings
 config.initial_cols = 120
 config.initial_rows = 32
-
---config.window_decorations = "RESIZE"
 config.window_decorations = "TITLE | RESIZE"
-
 -- BlinkingBar, BlinkingBlock, BlinkingUnderline, SteadyBar, SteadyBlock, SteadyUnderline
 config.default_cursor_style = 'BlinkingBlock'
 
+config.font = wezterm.font "Hack Nerd Font"
+--config.font = wezterm.font "CaskaydiaMono Nerd Font"
+--config.font = wezterm.font "FiraCode Nerd Font Mono"
+--config.font = wezterm.font "Inconsolata Nerd Font"          -- too small
+--config.font = wezterm.font "Iosevka Nerd Font"              -- too skinny and tall
+--config.font = wezterm.font "OpenDyslexic Nerd Font"         -- spaced too far apart
+--config.font = wezterm.font "MesloLGM Nerd Font"
+--config.font = wezterm.font "NotoSans Nerd Font"             -- spaced too far apart
+--config.font = wezterm.font "SauceCodePro Nerd Font"         -- good!
+--config.font = wezterm.font "Hasklug Nerd Font"              -- good!
+--config.font = wezterm.font "CodeNewRoman Nerd Font"         -- good, but small
+
+
+--config.font = wezterm.font "DroidSansM Nerd Font"
+--config.font = wezterm.font "FiraCode Nerd Font"
+--config.font = wezterm.font "Inconsolata LGC Nerd Font"
+--config.font = wezterm.font "InconsolataGo Nerd Font"
+--config.font = wezterm.font "IosevkaTerm Nerd Font"
+--config.font = wezterm.font "IosevkaTermSlab Nerd Font"
+--config.font = wezterm.font "MesloLGL Nerd Font"
+--config.font = wezterm.font "MesloLGLDZ Nerd Font"
+--config.font = wezterm.font "MesloLGMDZ Nerd Font"
+--config.font = wezterm.font "MesloLGS Nerd Font"
+--config.font = wezterm.font "MesloLGSDZ Nerd Font"
+--config.font = wezterm.font "NotoSansM Nerd Font"
+--config.font = wezterm.font "NotoSerif Nerd Font"
+--config.font = wezterm.font "OpenDyslexicAlt Nerd Font"
+--config.font = wezterm.font "OpenDyslexicM Nerd Font"
+
+
+-- Color Scheme
 --config.color_scheme = 'Purple People Eater (Gogh)'
 --config.color_scheme = 'Pixiefloss (Gogh)'
 --config.color_scheme = 'Peppermint'
@@ -27,7 +58,6 @@ config.default_cursor_style = 'BlinkingBlock'
 --config.color_scheme = 'Aura (Gogh)'
 --config.color_scheme = 'Aurora'
 --config.color_scheme = 'Japanesque (Gogh)'
-
 
 --config.color_scheme = 'Jellybeans (Gogh)'
 --config.color_scheme = 'JWR dark (terminal.sexy)'
