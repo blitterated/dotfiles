56fdc513cf49338ddbad0666c418ce8e67ad3822
blitterated <blitterated@proton.me>
2026-03-04 16:20:33 -0600

    Remove redundant neovim configuration

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,22 +1,9 @@
+-- Make sure to set up vim options before loading
+-- lazy.nvim so that mappings are correct.
 require("vim-options")
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- Bootstrap lazy.nvim from ~/.config/nvim/lua/config/lazy.lua.
+require("config.lazy")
 
-require("lazy").setup("plugins")
-
+-- Set colors for line numbers in left gutter.
 require("linenumber-colors")
