cd11e259016fa0755a9c36c9b7ce4868c38cc2e1
blitterated <blitterated@proton.me>
2026-03-04 23:20:46 -0600

    Better line numbering

--- home/dot_config/nvim/lua/vim-options.lua
+++ home/dot_config/nvim/lua/vim-options.lua
@@ -2,8 +2,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Show line numbers by default
-vim.cmd("set number")
+-- Show line numbers
+vim.o.number = true
+vim.o.relativenumber = true
 
 -- Configure Tabs
 vim.cmd([[
