b17581e7d144206dc36012e307aac736868c596c
blitterated <blitterated@proton.me>
2026-03-08 23:57:44 -0500

    Brighten line numbers in Neovim

--- home/dot_config/nvim/lua/vim-options.lua
+++ home/dot_config/nvim/lua/vim-options.lua
@@ -23,8 +23,10 @@ vim.cmd("set list listchars=tab:\\ \\ ,trail:·")
 -- Turn on cursorline
 vim.cmd("set cursorline")
 
--- Set cursorline number colors in left gutter
+-- Set  number colors in left gutter
+vim.api.nvim_set_hl(0, 'LineNrAbove',  { fg='#A1BEEF', bold=true })
 vim.api.nvim_set_hl(0, 'LineNr',       { fg='#A1BEEF', bold=true })
+vim.api.nvim_set_hl(0, 'LineNrBelow',  { fg='#A1BEEF', bold=true })
 vim.api.nvim_set_hl(0, 'CursorLineNr', { fg='#DEEAFF', bold=true })
 
 -- Turn off search highlighting with Enter key
