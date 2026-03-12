de4071e844ee4a2ead68e09809cf02e7f439c080
blitterated <blitterated@proton.me>
2026-03-12 15:47:27 -0500

    Set color for absolute line numbers in Neovim

--- home/dot_config/nvim/lua/post-theme-linenumber-colors.lua
+++ home/dot_config/nvim/lua/post-theme-linenumber-colors.lua
@@ -1,4 +1,13 @@
--- Set  number colors in left gutter
+-- Set line number colors in left gutter
+
+-- Relative line numbers above the cursor line
 vim.api.nvim_set_hl(0, 'LineNrAbove',  { fg='#FFFF00', bold=true })
+
+-- Absolute line numbers above and below cursor line
+vim.api.nvim_set_hl(0, 'LineNr',       { fg='#AAAAFF', bold=true })
+
+-- Absolute line number on the cursor line
 vim.api.nvim_set_hl(0, 'CursorLineNr', { fg='#FFAAAA', bold=true })
+
+-- Relative line numbers below the cursor line
 vim.api.nvim_set_hl(0, 'LineNrBelow',  { fg='#AAFFFF', bold=true })
