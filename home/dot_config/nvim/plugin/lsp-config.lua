1fa3a18d9153e9ec458dd8890134f8fcc78dad0d
blitterated <blitterated@proton.me>
2026-04-15 03:09:53 -0500

    Configure LSP autocompletion
    https://youtu.be/tdhxpn1XdjQ?t=409

--- home/dot_config/nvim/plugin/lsp-config.lua
+++ home/dot_config/nvim/plugin/lsp-config.lua
@@ -1,3 +1,21 @@
 
 -- "lua_ls" is from the filename: lsp/lua_ls.lua
 vim.lsp.enable({ "lua_ls" })
+
+
+
+-- Enable autocompletion if the LSP supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+
+      -- Show autocompletion manually with Ctrl-space
+      vim.keymap.set('i', '<C-Space>', function()
+        vim.lsp.completion.get()
+      end)
+    end
+  end,
+})
