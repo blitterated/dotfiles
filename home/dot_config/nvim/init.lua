a6faba7892a25bed378d8b1a7a0b44741f9d3614
blitterated <blitterated@proton.me>
2026-04-20 19:39:45 -0500

    Ensure line number colors are set after plugins

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,6 +1,3 @@
 -- Make sure to set up vim options before loading
 -- lazy.nvim so that mappings are correct.
 require("vim-options")
-
--- Set colors for line numbers in left gutter.
-require("post-theme-linenumber-colors")
